69e6b00c4b1024cf7298bfa52e7be40745152179
eileenlimur <ailin.li@hotmail.com>
2020-06-26 02:30:51 +0000

    convert getAllReservations to node postgres query

--- 1_queries/4_all_my_reservations.sql
+++ 1_queries/4_all_my_reservations.sql
@@ -2,7 +2,7 @@ SELECT reservations.*, properties.*, AVG(rating) as average_rating
 FROM reservations
 JOIN properties ON properties.id = reservations.property_id
 JOIN property_reviews ON properties.id = property_reviews.property_id
-WHERE reservations.guest_id = 1
+WHERE reservations.guest_id = 3
 AND now()::date > start_date
 GROUP BY properties.id, reservations.id
 ORDER BY start_date
